299203e62dee491af7897d8e21475fe94b5c35a3
super.rui <1984485320@qq.com>
2022-07-13 11:52:48 +0800

    '初始化'

---
+++ src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  title: '韭菜加工厂',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: true
+}
